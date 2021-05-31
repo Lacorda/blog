cd15f8c07430aee9ffc67c81b4d3e1ad27a9fbc6
小兔在冲浪 <1444586297@qq.com>
2021-05-31 14:12:27 +0800

    项目经验-淘宝小程序埋点

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -9,18 +9,11 @@ module.exports = [
         text: '基础知识点',
         items: [
           { text: 'JavaScript', link: '/pages/4456e3/' },
-          { text: 'ES6', link: '/pages/a0057b/' },
-          { text: 'HTML与CSS', link: '/pages/a0057b/' },
-          { text: 'JS设计模式', link: '/pages/a0057b/' },
-        ],
-      },
-      {
-        text: '我的博文',
-        items: [
-          { text: 'Vue', link: '/pages/a0057b/' },
-          { text: 'React', link: '/pages/a0057b/' },
-          { text: 'Webpack', link: '/pages/a0057b/' },
-          { text: 'Nodejs', link: '/pages/a0057b/' },
+          { text: 'ES6', link: '/pages/d45d68/' },
+          { text: 'HTML与CSS', link: '/pages/0135b8/' },
+          { text: 'JS设计模式', link: '/pages/e1006e/' },
+          { text: 'Web API', link: '/pages/5b3b9e/' },
+        //   { text: '性能', link: '/pages/5b3b9e/' },
         ],
       },
     ],
@@ -30,7 +23,7 @@ module.exports = [
     link: '/algorithm/',
     items: [
         { text: '排序算法', link: '/pages/7d4d69/' },
-        { text: 'LeetCode', link: '/pages/a0057b/' },
+        // { text: 'LeetCode', link: '/pages/a0057b/' },
         { text: '算法面试题解', link: '/pages/a0057b/' },
     ],
   },
@@ -38,17 +31,18 @@ module.exports = [
     text: '插件|框架',
     link: '/services/',
     items: [
-        { text: 'Vue', link: '/pages/a0057b/' },
-        { text: 'React', link: '/pages/a0057b/' },
+        // { text: 'Vue', link: '/pages/a0057b/' },
+        // { text: 'React', link: '/pages/a0057b/' },
     ],
   },
   {
     text: '项目|工具',
     link: '/tools/',
     items: [
-      { text: '项目搭建', link: '/pages/docs/' },
-      { text: '技术文档', link: '/pages/docs/' },
-      { text: 'Git技巧', link: '/pages/git/' },
+      { text: '项目笔记', link: '/pages/3f852b/' },
+    //   { text: '项目搭建', link: '/pages/docs/' },
+    //   { text: '技术文档', link: '/pages/docs/' },
+    //   { text: 'Git技巧', link: '/pages/git/' },
     ],
   },
   {
